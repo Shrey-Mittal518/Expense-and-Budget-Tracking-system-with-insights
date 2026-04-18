eccb4a08fa8bfeed4a88392a45e765d888c3e1b7
Shrey Mittal <shreymittal349@gmail.com>
2026-04-18 16:05:05 +0530

    new change

--- advanced_expense_tracker/run.py
+++ advanced_expense_tracker/run.py
@@ -13,4 +13,4 @@ from app import create_app
 if __name__ == '__main__':
     app = create_app()
     port = int(os.getenv('PORT', 8000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port =port, debug=True)
